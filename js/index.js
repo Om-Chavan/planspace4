a3277e9358664f6c8c420e9bd047acb23b826652
ProgrammerOm <79402285+Om-Chavan@users.noreply.github.com>
2023-09-17 14:01:05 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function facebook() {
+    window.open("https://www.facebook.com/profile.php?id=61550932072229&mibextid=ZbWKwL", "_blank");
+}
+
+function insta() {
+    window.open("https://instagram.com/planspace.in?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D", "_blank");
+}
+
+function yt() {
+    window.open("https://www.facebook.com/profile.php?id=61550932072229&mibextid=ZbWKwL", "_blank");
+}
+
+function twitter() {
+    window.open("https://www.facebook.com/profile.php?id=61550932072229&mibextid=ZbWKwL", "_blank");
+}
